ef74988b2af7420a71132a57ede7f405f7bd4877
nirbhaynaruka <49834120+nirbhaynaruka@users.noreply.github.com>
2019-09-21 05:58:54 +0530

    n

--- js/index.js
+++ js/index.js
@@ -8,29 +8,36 @@ window.onscroll = function () {
   var header = document.getElementById("fadein");
   function myFunction() {
    
-    if (document.body.scrollTop > 320 || document.documentElement.scrollTop > 320) {
-      header.classList.add("hidediv");
+    if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
+      // header.classList.add("hidediv");
+      $("#fadein").fadeOut(200);
     } else {
-      header.classList.remove("hidediv");
+      // header.classList.remove("hidediv");
+      $("#fadein").fadeIn(500);
     }
   }
   
   var header1 = document.getElementById("myHeader");
+  var extra = document.getElementById("");
+
+
   function myFunctionHeader() {
    
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-      header1.classList.add("headertoggle");
+      header1.classList.add("bg-dark1");
+      extra.classList.remove("extra");
     } else {
-      header1.classList.remove("headertoggle");
+      header1.classList.remove("bg-dark1");
+      extra.classList.remove("extra");
 
     }
 
   var hidehover = document.getElementsByClassName("hovereffect");
-    function hidediv(){
-      if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        hidediv.classList.add("hidediv");
-      } else {
-        hidediv.classList.remove("hovereffect");
-      }
-    }
+    // function hidediv(){
+    //   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    //     hidediv.classList.add("hidediv");
+    //   } else {
+    //     hidediv.classList.remove("hovereffect");
+    //   }
+    // }
   }
